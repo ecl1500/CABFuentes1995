766a5a9be3b9c8489cbb62a729eda7be45dfc2d4
emilio campo <ecamleo496a@isidrodearceneguiycarmona.es>
2024-05-20 19:44:59 +0200

    Lunes 20

--- PUBLIC/PHP/verificarAcceso.php
+++ PUBLIC/PHP/verificarAcceso.php
@@ -3,21 +3,21 @@ include 'conexion.php'; // Incluir el archivo de conexión a la base de datos
 
 // Verificar si se enviaron datos del formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id = $_POST['id'];
+    $email = $_POST['email'];
     $pin = $_POST['pin'];
 
     // Consulta SQL para verificar las credenciales del usuario
-    $query = "SELECT * FROM usuarios WHERE ID = '$id' AND pin = '$pin'";
+    $query = "SELECT * FROM usuarios WHERE email = '$email' AND pin = '$pin'";
     $result = mysqli_query($conexion, $query);
 
     if (mysqli_num_rows($result) == 1) {
         // Iniciar sesión y redirigir al usuario a la página de inicio
         session_start();
         $_SESSION['loggedin'] = true;
-        $_SESSION['id'] = $id;
+        $_SESSION['email'] = $email;
         header("location: inicio.php");
     } else {
-        echo '<script>alert("ID o PIN incorrecto");</script>';
+        echo '<script>alert("Email o PIN incorrecto");</script>';
         echo '<script>setTimeout(function() { window.location.href = "../../login.html"; }, 100);</script>';
 
     }
