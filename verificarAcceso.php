f2b0dffcd69d9ecc4780bf243de762567c9b3c37
emilio campo <ecamleo496a@isidrodearceneguiycarmona.es>
2024-06-07 12:56:40 +0200

    FINAL

--- verificarAcceso.php
+++ verificarAcceso.php
@@ -1,9 +1,4 @@
 <?php
-// Agregar encabezados de seguridad
-header("Content-Security-Policy: default-src 'self';");
-header("X-Frame-Options: DENY");
-header("X-Content-Type-Options: nosniff");
-
 // Verificar si el archivo de conexión a la base de datos existe y luego incluirlo
 if (file_exists(__DIR__ . '/PUBLIC/PHP/conexion.php')) {
     include __DIR__ . '/PUBLIC/PHP/conexion.php';
@@ -33,17 +28,16 @@ $usuario = $result->fetch_assoc();
 
 // Verificar si las credenciales son correctas
 if ($usuario && password_verify($pin, $usuario['pin_hash'])) {
+    
     // Iniciar una sesión y almacenar información del usuario
     session_start();
-    $_SESSION['user_id'] = $usuario['id'];
+    $_SESSION['idUsuario'] = $usuario['id'];
     $_SESSION['email'] = $usuario['email'];
     // Redirigir al usuario a logueado.php
     header("Location: /CABFuentes1995/logueado.php");
-    exit();
 } else {
     // Mostrar una alerta y redirigir al usuario a login.html
     echo "<script>alert('Las credenciales no son válidas'); window.location.href = 'login.html';</script>";
-    exit();
 }
 
 // Cerrar la consulta y la conexión
